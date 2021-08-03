84725f5503e2267aa764125ff0d2c3153140994b
NiranjanVS <niranjanvs03@gmail.com>
2021-08-03 19:50:57 +0530

    Day 3

--- Day_3/Assignment/Bank/src/com/telstra/tse/Account.java
+++ Day_3/Assignment/Bank/src/com/telstra/tse/Account.java
@@ -2,4 +2,36 @@ package com.telstra.tse;
 
 public class Account {
 
+	private final int AccID;
+	private String AccType;
+	private double AccBalance = 0;
+	//private double CreditAmount = 0;
+	
+	public Account(int accID, String accType, double accBalance) {
+		super();
+		AccID = accID;
+		AccType = accType;
+		AccBalance = accBalance;
+	}
+
+	public void deposit(double depositAmt) {
+		if(depositAmt<=0) 
+			System.out.println("Deposit Not Possible because Deposit Amount is zero or less than zero");
+		else {
+			AccBalance+=depositAmt;
+			System.out.println("Deposit Successful");
+			System.out.println("Balance Amount - "+AccBalance);
+		}
+		
+	}
+	
+	public void withdrawal(double withdrawalAmt) {
+		if((AccBalance-withdrawalAmt)<0)
+			System.out.println("Insufficient Balance - Withdrawal Not possible");
+		else {
+			AccBalance-=withdrawalAmt;
+			System.out.println("Balance Amount - "+AccBalance);
+		}
+	}
+	
 }
--- Day_3/Assignment/Bank/src/com/telstra/tse/Customer.java
+++ Day_3/Assignment/Bank/src/com/telstra/tse/Customer.java
@@ -1,5 +1,8 @@
 package com.telstra.tse;
 
 public class Customer {
+	private int customerID;
+	private int customerName;
+	private Account customerAcc;
 
 }
